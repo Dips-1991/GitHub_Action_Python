ba64dbbe5ded3c7de9f7064d67532d585f16338b
Deepak Patil <Patildipak0791@gmail.com>
2025-07-11 02:47:20 +0000

    Pull request

--- Python/app.py
+++ Python/app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World! I am a Successful DevOps/AI Engineer</h1>'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
